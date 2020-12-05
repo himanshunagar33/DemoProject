e1dbc023d5953dcb68e9766592988088aea003e3
himanshunagar33 <himanshunagar33@gmail.com>
2020-12-05 11:58:04 +0530

    Create sizeVerification.java

---
+++ JavaCrashCourse/src/doubts/sizeVerification.java
@@ -0,0 +1,12 @@
+package doubts;
+
+import java.util.ArrayList;
+
+public class sizeVerification {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> al= new ArrayList<>();
+		System.out.println(al.size());
+	}
+}
